fadbed997479b6be99ccf967f60599f6150d83ea
TeamRory <rorycteam@gmail.com>
2014-06-05 14:44:21 -0400

    Finished up the recipe and SingleRecipe classes. Now I am makeing a SingleIngredient class and amending the Ingredient class.

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,4 +1,5 @@
 class SingleRecipe
+  attr_reader :id, :name, :instructions, :description
   def initialize(id, name, instructions, description)
     @id = id
     @name = name
@@ -6,35 +7,26 @@ class SingleRecipe
     @description = description
   end
 
-  def id
-    @id
-  end
+  def ingredients
 
-  def name
-    @name
-  end
 end
 
 
  class Recipe
-  def initialize
-    @recipes = []
-    query = self.db_connection do |conn|
-      conn.exec("SELECT name, id FROM recipes WHERE description IS NOT NULL ORDER BY name;")
+  def self.all
+    recipes = []
+       query = self.db_connection do |conn|
+      conn.exec("SELECT * FROM recipes WHERE description IS NOT NULL ORDER BY name;")
     end
-      query.each do |recipe|
+       query.each do |recipe|
         id = recipe['id']
         name = recipe['name']
         instructions = recipe['instructions']
         description = recipe['description']
         new_recipe = SingleRecipe.new(id, name, instructions, description)
-        @recipes << new_recipe
+        recipes << new_recipe
       end
-  end
-
-
-  def self.all
-    @recipes
+      recipes
   end
 
   def self.db_connection
@@ -48,5 +40,15 @@ end
 
   def each
     self.all.each
+  end
+
+  def self.find(id)
+    found = nil
+    self.all.each do |recipe|
+      if recipe.id == id
+        found = recipe
+      end
+    end
+    found
   end
 end
